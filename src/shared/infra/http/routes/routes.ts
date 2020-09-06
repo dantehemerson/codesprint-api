eac11ba1ab140e1c14d8ca604b1068fd7365a07c
stevescruz <steve.silverio.cruz@gmail.com>
2020-09-06 07:02:15 -0400

    Feature: Updated Routes main file with Categories Routes

--- src/shared/infra/http/routes/routes.ts
+++ src/shared/infra/http/routes/routes.ts
@@ -1,6 +1,7 @@
 import { usersRouter } from '@modules/users/http/routes/users.routes';
+import { categoriesRouter } from '@modules/categories/http/routes/categories.routes';
 import { Router } from 'express';
 
 export const routes = Router();
-
 routes.use('/users', usersRouter);
+routes.use('/categories', categoriesRouter);
