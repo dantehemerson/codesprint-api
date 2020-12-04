244f5cef9df9338f8156ee98ad8251dd438856df
Dante Calderon <mail@dantecalderon.dev>
2020-12-04 02:11:52 -0500

    Add swagger

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,30 +1,39 @@
-import 'reflect-metadata';
 import '@shared/container';
-import { connection } from '@shared/infra/typeorm';
-import { createExpressServer, Action } from 'routing-controllers';
-import { join } from 'path';
-import { Application } from 'express';
-import { Connection } from 'typeorm';
-import passport from 'passport';
-import { jwtStrategy } from '@shared/http/strategies/jwt.strategy';
 import { authorizationChecker } from '@shared/http/routing-controllers/authorization-checker.helper';
+import { jwtStrategy } from '@shared/http/strategies/jwt.strategy';
+import { connection } from '@shared/infra/typeorm';
+import { defaultMetadataStorage } from 'class-transformer/storage';
+import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
+import { Application } from 'express';
+import passport from 'passport';
+import { join } from 'path';
+import 'reflect-metadata';
+import {
+  Action,
+  createExpressServer,
+  getMetadataArgsStorage,
+  RoutingControllersOptions,
+} from 'routing-controllers';
+import { routingControllersToSpec } from 'routing-controllers-openapi';
+import { Connection } from 'typeorm';
+import * as swaggerUiExpress from 'swagger-ui-express';
 
 export class App {
   public typormConnection: Connection;
-
   private expressApp: Application;
+  private readonly routingControllersOptions: RoutingControllersOptions = {
+    controllers: [
+      join(process.cwd(), 'src/modules/**/http/controllers/*.controller.ts'),
+    ],
+    authorizationChecker,
+    currentUserChecker: async (action: Action) => action.request.user,
+  };
 
   async init() {
     passport.use(jwtStrategy());
 
     this.typormConnection = await connection;
-    this.expressApp = createExpressServer({
-      controllers: [
-        join(process.cwd(), 'src/modules/**/http/controllers/*.controller.ts'),
-      ],
-      authorizationChecker,
-      currentUserChecker: async (action: Action) => action.request.user,
-    });
+    this.expressApp = createExpressServer(this.routingControllersOptions);
 
     this.initMiddleware();
 
@@ -37,6 +46,47 @@ export class App {
 
   private initMiddleware() {
     this.expressApp.use(passport.initialize());
+    this.useOpenAPI();
+  }
+
+  private useOpenAPI() {
+    const schemas = validationMetadatasToSchemas({
+      classTransformerMetadataStorage: defaultMetadataStorage,
+      refPointerPrefix: '#/components/schemas/',
+    });
+
+    const storage = getMetadataArgsStorage();
+
+    const spec = routingControllersToSpec(
+      storage,
+      this.routingControllersOptions,
+      {
+        components: {
+          schemas,
+          securitySchemes: {
+            bearerAuth: {
+              scheme: 'bearer',
+              bearerFormat: 'JWT',
+              type: 'http',
+            },
+          },
+        },
+        info: {
+          description:
+            'It provides an overview around our available endpoints, *you need to be authenticated to use our API*',
+          title: 'CodeSprint API',
+          version: '0.1.0',
+        },
+      },
+    );
+
+    this.expressApp.use(
+      '/docs',
+      swaggerUiExpress.serve,
+      swaggerUiExpress.setup(spec, {
+        customSiteTitle: 'CodeSprint API',
+      }),
+    );
   }
 
   getServer() {
