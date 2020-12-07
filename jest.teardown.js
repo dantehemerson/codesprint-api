678cc093a6c2d4bcaebd1459e80e481dd53d728d
Dante Calderon <mail@dantecalderon.dev>
2020-12-07 01:43:22 -0500

    Global app server

--- jest.teardown.js
+++ jest.teardown.js
@@ -1,8 +1,10 @@
 const { execSync } = require('child_process');
+const { teardown } = require('./src/__tests__/helpers/jest-global.setup')
 
-module.exports = function () {
+module.exports = async function () {
 	const stdout = execSync(
 		'docker-compose -p docker_app_test -f docker-compose.test.yml down',
-	);
+  );
+  await teardown()
 	console.log(stdout.toString('utf-8'));
 };
